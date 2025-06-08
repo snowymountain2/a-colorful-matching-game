c0f0d941bb5255ae558baeb36abbb5c90b63cda5
john-paul <johnpaulcody8@gmail.com>
2025-06-07 20:47:32 -0400

    sdf

--- src/Components/high-score-screen/HighScoreScreen.jsx
+++ src/Components/high-score-screen/HighScoreScreen.jsx
@@ -88,7 +88,7 @@ export default function HighScoreScreen({
 
   async function getHighScores() {
     const { data } = await supabase.from("highscores").select();
-    console.log(data);
+    console.log("hi", data);
     const formattedData = data
       .filter((highscore) => highscore.mode === gameSelectionValues.gameMode)
       .sort(({ msScore: a }, { msScore: b }) => a - b);
